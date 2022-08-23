96a5f42c530e64aa086985aa30cf26c1e512b2cc
soulonefirst <andryshka.88@mail.ru>
2022-08-23 17:17:26 +0300

    Delete wreks and cutscene

--- hide-technologies.lua
+++ hide-technologies.lua
@@ -7,7 +7,24 @@ end
 data.raw["unit-spawner"]["biter-spawner"].dying_trigger_effect = {type ="script",effect_id ="spawner_destroyed"}
 data.raw["unit-spawner"]["spitter-spawner"].dying_trigger_effect = {type ="script",effect_id ="spawner_destroyed"}
 
+local crash_site_objects ={"big-ship-wreck-1",
+"big-ship-wreck-2",
+"big-ship-wreck-3",
+"crash-site-chest-",
+"crash-site-chest-",
+"crash-site-spaceship",
+"crash-site-spaceship-wreck-big-1",
+"crash-site-spaceship-wreck-big-2",
+"crash-site-spaceship-wreck-medium-1",
+"crash-site-spaceship-wreck-medium-2",
+"crash-site-spaceship-wreck-medium-3",
+"factorio-logo-11tiles",
+"factorio-logo-16tiles",
+"factorio-logo-22tiles",}
 
+for _, value in ipairs(crash_site_objects) do
+  
+end
 data:extend{
   {
     type = "item",
